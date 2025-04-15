2f7df5a29ae92b2b7eb85e19bc13d42a3b254cc3
Abilash Suresh <abilash.suresh199@gmail.com>
2025-04-15 02:55:00 -0400

    codecrafters submit [skip ci]

--- app/replica_cmds.go
+++ app/replica_cmds.go
@@ -54,7 +54,7 @@ func (h *RedisServer) handleReplicaSET(conn net.Conn, args []string) error {
 }
 
 func (h *RedisServer) handleReplicaREPLCONF(conn net.Conn, args []string) error {
-	_, err := conn.Write([]byte(h.protocol.stringToArray([]string{"REPLCONF", "ACK", string(h.config.MasterReplOffset)})))
+	_, err := conn.Write([]byte(h.protocol.stringToArray([]string{"REPLCONF", "ACK", string(int(h.config.MasterReplOffset))})))
 	return err
 }
 
